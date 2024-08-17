ae9bfb8ebc0df8c6b0caae7ba40e1290ed9640db
Abrahmbek <145168869+Abrahmbek@users.noreply.github.com>
2024-08-18 08:48:53 +0900

    feat config.js and deploy.sh created

---
+++ process.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps : [{
+        name: "toolscart.kr",
+        cwd: "./",
+        script: "./server.js",
+        watch: true,
+    env_production: {
+          NODE_ENV: "production"
+        },
+        env_development: {
+          NODE_ENV: "development"
+        },
+        instances: 2,
+        exec_mode: "cluster",
+  }]
+}
