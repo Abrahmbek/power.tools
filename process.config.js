7f669adf448677b3615f6728f95a0c75d88bc004
Abrahmbek <145168869+Abrahmbek@users.noreply.github.com>
2024-08-20 22:09:01 +0900

    fix deploy ps

--- process.config.js
+++ process.config.js
@@ -1,16 +1,17 @@
 module.exports = {
-  apps : [{
-        name: "toolscart.kr",
-        cwd: "./",
-        script: "./server.js",
-        watch: true,
+  apps: [{
+    name: "toolscart.kr",
+    cwd: "./",
+    script: "./server.js",
+    watch: true,
     env_production: {
-          NODE_ENV: "production"
-        },
-        env_development: {
-          NODE_ENV: "development"
-        },
-        instances: 2,
-        exec_mode: "cluster",
-  }]
-}
+      NODE_ENV: "production"
+    },
+    env_development: {
+      NODE_ENV: "development"
+    },
+    instances: 2,
+    exec_mode: "cluster",
+  },
+  ],
+};
