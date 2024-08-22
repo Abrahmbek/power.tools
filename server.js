1c06d65941c14a60fce105d0ed9785b975e1b135
Abrahmbek <145168869+Abrahmbek@users.noreply.github.com>
2024-08-22 12:42:19 +0900

    fixed app.js package.json server.js

--- server.js
+++ server.js
@@ -1,29 +1,7 @@
-<<<<<<< HEAD
-const http = require( "http");    // http ni require qilib talab qilib oldik
-const mongodb = require("mongodb");       // mongo db ni talab qilib qilib oldik
-
-let db;                   //let ozgaruchisi yordamida mongo db elon qilib oldik
-const connectionString =
-"mongodb+srv://Abrahmbek:RawWbrCBIC3CTf87@cluster0.6kdla7u.mongodb.net/powerTools";     //  mongo db ga kalit ulanish kodi
+//const http = require( "http");    // http ni require qilib talab qilib oldik
+//const mongodb = require("mongodb");       // mongo db ni talab qilib qilib oldik
 
 
-mongodb.connect(                        //mongo db ni 3 xil yol bilan elon qilib oldik 
-    connectionString,                    //mongo db ni connect methodni 3 ta qiymat qabul qilyapti
-    {
-        useNewUrlParser: true,               // docs da gi bilan bir hil 
-        useUnifiedTopology: true,
-    },
-    (err, client) => {                                             //callback function chaqiramiz agar error bolsa bizga errorni korsat
-        if(err) console.log("ERROR on connection MongoDB ");         
-        else {                                                    // aks holda mongo db ga ulansin
-            console.log("Mongo connection succeed");       
-                                                                // client ni ornida  "data " yozsa ham boladi bu yerda client yozilgan
-            module.exports = client;                               // mongodb dan app.js ga path qilyapmiz
-           
-            const app = require("./app");                  // app js ni sorab oldik shu yerda ishga tushiryapmiz
-            const server = http.createServer(app);             // http server yartib ichida app ni chaqirib olyapmiz
-            let PORT = 3001;                                    //port 3000 da korsat deyapmiz
-=======
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -52,7 +30,7 @@ mongoose.connect(
             const server = require("./app");                  
            // const server = http.createServer(app);             
             let PORT = process.env.PORT || 3001;                                  
->>>>>>> develop
+
            
             server.listen(PORT, function() {                   
                 console.log(
